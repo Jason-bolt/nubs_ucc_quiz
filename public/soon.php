9e3bd0777eb61107f841475520baae5357db496c
Jason-bolt <58566161+Jason-bolt@users.noreply.github.com>
2020-05-12 22:17:03 +0000

    Add files via upload
    Adding a soon page

---
+++ public/soon.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Quiz</title>
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<section class="container py-1 text-center">
+	<h4><img src="nubs_logo.png" width="60">NUBS UCC - <span style="font-size: 20px;">BIBLE QUIZ</span></h4>
+	<h6><u>PROVERBS CHAPTER 11</u></h6>
+</section>
+
+
+<section class="container py-5 text-center">
+	<div class="card py-4">
+		<h5>Quiz on Proverbs chapter 12 will start at 10am</h5>
+	</div>
+</section>
+
+
+
+
+<footer class="text-center" style="font-family: Courier New; background-color: #ebebeb; padding: 10px; color: #999;">
+	<cite>&copy;NUBS UCC DEVELOPERS</cite>
+</footer>
+
+</body>
+</html>
