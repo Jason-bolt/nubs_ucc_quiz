c960e5d2f857ea20e93d5806ecdc9a93505b3b8b
Jason-bolt <58566161+Jason-bolt@users.noreply.github.com>
2020-05-13 10:53:48 +0000

    Add files via upload

--- public/submit.php
+++ public/submit.php
@@ -12,7 +12,7 @@ $user_answers = []; // to hold correct answers
 $score = 0;
 
 // fetcing results of user
-for ($i=1; $i <= 33; $i++) { 
+for ($i=1; $i <= 39; $i++) { 
 	$index = "question" . $i;
 	${"question" . $i} = $_POST[$index];
 
