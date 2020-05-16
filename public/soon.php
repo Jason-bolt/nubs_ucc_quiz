af22438e0e468690ec0e204e202ff522f4d2f0df
Jason-bolt <58566161+Jason-bolt@users.noreply.github.com>
2020-05-16 20:54:40 +0000

    Add files via upload

--- public/soon.php
+++ public/soon.php
@@ -24,7 +24,7 @@
 
 <section class="container py-5 text-center">
 	<div class="card py-4">
-		<h5>Quiz on Proverbs chapter 15 will start at 10am</h5>
+		<h5>Sorry but the quiz will resume on Tuesday, stay blessed</h5>
 	</div>
 </section>
 
