d85d3ba3c97554265f9fb24d896623bbd46b25fa
Jason-bolt <58566161+Jason-bolt@users.noreply.github.com>
2020-05-17 02:38:49 +0000

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php session_start();
-	header("Location: soon.php");
+	// header("Location: soon.php");
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,7 +29,7 @@
 <section class="container py-1 text-center">
 	<h4><img src="nubs_logo.png" width="60">NUBS UCC - <span style="font-size: 20px;">BIBLE QUIZ</span></h4>
 	<p style="color: red; font-size: 16px;">The quiz ends at 10pm</p>
-	<h6><u>PROVERBS CHAPTER 15</u></h6>
+	<h6><u>PROVERBS CHAPTER 16</u></h6>
 </section>
 
 <!-- name not inputted so display form -->
@@ -122,162 +122,186 @@
 
 <!-- 
 
-CHAPTER 15
+CHAPTER 16
 
-1. What will turn away wrath?
-(A) a good answer
-(B) soft answer
-(C) an honest answer
+1. To whom belong the preparations of the heart?
+(A) wise man
+(B) Man
+(C) Father
 
-2. How does the tongue of the wise use knowledge?
-(A) with good report
-(B) in abundance
-(C) rightly
+2. Who has the answer of the tongue?
+(A) The righteous
+(B) Wise man
+(C) The Lord
 
-3. Where does the eyes of the Lord watch evil and good?
-(A) every place
-(B) in secret places
-(C) above
+3. In the eyes of a man his ways are what?
+(A) Pure
+(B) Good
+(C) Righteous
 
-4. What is a wholesome tongue?
-(A) tongues of life
-(B) fountain of life
-(C) tree of life
+4. Who will judge the spirits?
+(A) The high Judge
+(B) The spiritual
+(C) The Lord
 
-5. Who is prudent?
-(A) he who receives correction
-(B) a person of knowledge
-(C) a faithful witness
+5. In order to establish our hearts, we are to do what to the Lord?
+(A) Commit one's works
+(B) Praise His name
+(C) recognize Him as Lord.
 
-6. There is much of what in the house of the righteous?
-(A) treasure
-(B) encouragement
-(C) wise men
+6. Who is made for the day of doom?
+(A) The wicked
+(B) The proud
+(C) The Scofferer
 
-7. What disperses knowledge?
-(A) joining NUBS what's up page
-(B) fountain of knowledge
-(C) lips of the wise
+7. Who is the everyone that is an abomination to the Lord?
+(A) Everyone who is wicked
+(B) Profane bubblers
+(C) Everyone proud of heart
 
-8. Whose prayer is God`s delight?
-(A) man of good countenance
-(B) the well informed
-(C) the upright
+8. In what is atonement provided?
+(A) In the house of the righteous
+(B) In every shedding of blood
+(C) In mercy and truth
 
-9. Whom does God love?
-(A) follower of righteousness
-(B) a witness
-(C) good listener
+9. By what does one depart from evil?
+(A) By the fear of the Lord
+(B) By moral standards
+(C) Obedience
 
+10. When can we expect God to make our enemies at peace with us?
+(A) When we seek peace 
+(B) When our ways please the Lord
+(C) When conflicts are resolved
 
-10. What is due him who forsakes the way?
-(A) death
-(B) discipline
-(C) bitterness
+11. When is a 'little' better?
+(A) With the right scale
+(B)With righteousness
+(C) When not stolen
 
-11. Whom does a scoffer NOT love?
-(A) words of encouragement
-(B) one who corrects him
-(C) the upright
+12. If a man's heart plans, who is the one who ultimately directs?
+(A) Wisdom
+(C) The fear of the Lord
+(C) The LORD
 
-12. What does a merry heart makes?
-(A) feast
-(B) great gains
-(C) cheerful countenance
+13. What is on the lips of the king?
+(A) Power
+(B) Divination
+(C) Commands
 
-13. What does the heart of understanding seek?
-(A) discernment
-(B) knowledge
-(A) mercy and love
+14. What kind of weights and scales are the Lord's?
+(A) Honest
+(B) Peaceful
+(C) Scales from above
 
-14. Who has a continual feast?
-(A) he who is of a merry heart
-(B) kings with great riches
-(C) ones from above
+15. By what is a throne established?
+(A) Laws
+(B) By kings
+(C) Righteousness
 
-15. Having a little can be better if it is with what?
-(C) happy ones
-(B) simple
-(C) the fear of the LORD
+16. What are the delight of kings? 
+(A) Counselor's advise
+(B)Righteous lips
+(C) An Honest Cup
 
-16. Where is being a vegetarian better?
-(A) with a wise man
-(B) where love is
-(C) in a house of the rich but wicked
+17. What will a wise man do to a king's wrath?
+(A) Understand it
+(B) Appease it
+(C) Discern it
 
-17. Who allays contention?
-(A)foolish ones
-(B) he who is slow to anger
-(C) those with good words
+18. What is a king's favor like?
+(A) Precious oil 
+(B) burning incense mixed with oil
+(C) A cloud of the latter rain
 
-18. The road of the upright is a what?
-(A) full of precious jewels
-(B) knowledge
-(C) highway
+19. What should be chosen rather than silver?
+(A) A Good name
+(B) Understanding
+(C) Gold
 
-19. Who despises his mother?
-(A) ungrateful son
-(B) foolish man
-(C) the evil
+20. What does the upright do by keeping the highway of departing from evil?
+(A) Guard his lips
+(B) Righteousness
+(C) Preserves his soul
 
-20. To whom is folly a joy?
-(A) an unfaithful witness
-(B) one who is easily angered
-(C)one destitute of discernment
+21. What goes before a fall? 
+(A) Haughty
+(B) Pride
+(C) Poverty
 
-21. Plans are established by having what?
-(A) multitude of counselors
-(B) plans of wisdom
-(C) good plans
+22. What is better than dividing spoil with the proud?
+(A) Honest scale without contention
+(B) Mastery
+(C) A humble spirit with the lowly
 
-22. When is a spoken word good?
-(A) when in good words
-(B) when in mercy and truth
-(C) in due season
+23. Who is the happy person?
+(A) Believing in one's heart
+(B)Trusts in the LORD
+(C) Wise Labour 
 
-23. What is below?
-(A) burried corpse
-(B) hell
-(C) valley
+24. What increases learning?
+(A) He who has dust from one's feet
+(B) Knowledge
+(C) Sweetness
 
-24. Who will destroy the house of the proud?
-(A) he who has dust from one's feet
-(B) a crowned prince
-(C) the LORD
+25. What is a wellspring of life?
+(A) Understanding
+(B) Discernment
+(C) Honesty
 
-25. Whose words are pleasant to the Lord?
-(A) the pure
-(B) a great man
-(C) a true witness
+26. What teaches the mouth of the wise?
+(A) One who has the fear of the LORD
+(B) The heart
+(C) The abandoned soul
 
-26. Who troubles his own house?
-(A) one who has Little
-(B) he who is greedy for gain
-(C) the abandoned soul
+27. What is sweetness to the soul?
+(A) Honey
+(B) Humility
+(C) Pleasant words
 
-27. Whose heart studies how to answer?
-(A) the heart of good intuition
-(B) one who has the truth
-(C) righteous
+28. The way of death may seem what to a man? 
+(A) Life
+(B) Right
+(C) Broad
 
-28. Whose prayer does the Lord hear?
-(A) men of knowledge
-(B) righteous
-(C) blameless men
+29. Who does a person labor for?
+(A) Riches
+(B) The LORD
+(C) For himself
 
-29. What makes bones healthy?
-(A) a faithful witness
-(B) meal from above
-(C) a good report
+30. What is on the lips of an ungodly man like a burning fire?
+(A) Evil
+(B) Words
+(C) Envy
 
-30. Who despises his own soul?
-(A) he who disdains instruction
-(B) a poor servant
-(C) the soul pleaser
+31. Who separates the best of friends?
+(A) His Maker
+(B) An advisor
+(C) A whisperer
 
-31. What comes before honour?
-(A) discernment
-(B) riches
-(C) humility
+32. Who entices and leads a person in a bad way?
+(A) The Unsuccessful
+(B) A violent man
+(C) the whisperer
+
+33. Why does he wink his eye?
+(A) To gain false witness
+(B) To cause separation
+(C) To devise perverse things
+
+34. The silver-haired head is a crown of glory only if it is found where?
+(A) in good intimacy
+(B) on the heard
+(C) In the way of righteousness
+
+35. He who rules his spirit is better than whom?
+(A) his master
+(B) He who takes a city
+(C) the mighty
+
+36. Who is the one who makes the final decision?
+(A)  Judge 
+(B) Cast of lap
+(C) The LORD
 
  -->
