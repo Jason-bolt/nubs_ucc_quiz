574c62da0ceb8de664280a4f001ea837ede9b4f0
Jason-bolt <58566161+Jason-bolt@users.noreply.github.com>
2020-06-05 23:33:45 +0000

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php session_start();
-	header("Location: soon.php");
+	// header("Location: soon.php");
 include('header.php');
 include_once('db.php'); 
 ?>
@@ -150,162 +150,200 @@ Array.from(radios).forEach(function(r){
 
 <!-- 
 
-ACTS CHAPTER FOUR
+ACTS CHAPTER FIVE
 
-1. As Peter and John spoke unto the people, what 3 groups came upon them? (4:1)
-(A) The priests, the captain of the temple, and the Sadducees
-(B) The high priest, the captain of the temple and Sadducees
-(C) The priests, the Captain of the temple and Pharisees
+1. What was the name of Ananias' wife? (5:1)
+(A) Saphire
+(B) Saphira
+(C) Shiphrah
 
-2. Why did these groups come against Peter and John? (4:2)
-(A) They were disturbed by their teachings and preaching
-(B) They wanted to stone them
-(C) They heard their names from afar
+2. Who filled Ananias' heart to lie? (5:3)
+(A) Satan
+(B) His wife
+(C) Himself
 
-3. But many of the people-Peter and John spoke to had the word and believed: about how many men were there that believed? (4:4)
-(A) 3000
-(B) 300
-(C) 5000
+3. Who did Peter say Ananias has lied to? (5:3)
+(A) The Apostles
+(B) His wife
+(C) Holy Spirit
 
-4. What did the groups who came against Peter and John, do to them? (4:3)
-(A) Asked them difficult questions
-(B) They put them in custody until the next day
-(C) They put them into custody until evening
+4. What happened to Ananias when Peter finished speaking? (5:5)
+(A) He ran off
+(B) He fell down and breathed his last
+(C) He was stoned to death
 
-5. What was the High Priests name at this time?(4:6)
-(A) Annas
-(B) Caiaphas
-(C) Alexander
+5. What happened to Ananias' wife? (5:10)
+(A) He fell down at the Apostles feet breathed her last and died
+(B) She lied
+(C) She was stoned
 
-6. (This is concerning-the healing of the lame man) Peter and John were set in the midst of the High Priest and asked what? (4:7)
-(A) By what power or by what name have you done this?
-(B) Where do you get your power from?
-(C) Where do you come from, who is your master?
+6. Sick people were laid in the street, hoping this might overshadow some of them?(5:15)
+(A) Holy Spirit
+(B) Pillar of cloud
+(C) Peter's shadow
 
-7. This is the '___ which was rejected by you ___, which has become the chief cornerstone.' (4:11)
-(A) Stone, builders
-(B) Building, builders
-(C) Wall, builders
+7. Who opened the door and freed the Apostles from prison? (5:19)
+(A) Captain of the prison
+(B) An angel of the Lord
+(C) The high priest
 
-8. Neither is there salvation in any other: For there is none other name under heaven given among men whereby we must be what?(4:12)
-(A) Mentioning
-(B) Saved
-(C) Praising
+8. Where were the Apostles found the next day? (5:25) 
+(A) In the upper room
+(B) In the cave
+(C) In the temple
 
-9. When they saw the boldness of Peter and John, they perceived what about them?(4:13)
-(A) They perceived that they were uneducated and untrained men
-(B) They perceived that they have been with Jesus
+9. When the council wanted to kill the Apostles, this man talked them out of it? (5:34)
+(A) Gamaliel
+(B) Annas
+(C) Gamali
+
+10. How many hours was it when Annanias's wife came in to lie also? (5:7)
+(A) 3 hours
+(B) 6 hours
+(C) 5 hours
+
+11. What fell upon the people that heard about Annanias? (5:5)
+(A) Tongue of fire
+(B) Great fear
+(C) Mantle of power
+
+12. “And the young men arose and __ him up, __him out, and ___ him.” (5:6) 
+(A) Carried, wrapped, burried
+(B) Carried, burried, wrapped
+(C) Wrapped, carried, burried
+
+13. Peter asked Ananias' wife why they had agreed together to tempt who? (5:9)
+(A) The Spirit of the Lord
+(B) The Apostles
+(C) The believers
+
+14. Who buried Ananias' wife? (5:10)
+(A) The Apostles
+(B) Great men
+(C) Young men
+
+15. Because Peter and the other Apostles were healing people of sickness and unclean spirits, what sect of people rose up against them and had them put into
+prison? (5:17-18)
+(A) Sadducees
+(B) Pharisees
 (C) Both of the options above
 
-10. But they marveled at Peter and John- Why?(4:13)
-(A) When they saw their boldness
-(B) When they perceived they were uneducated and untrained
-(C) Both of the options above
+16. The angel of the Lord told the Apostles:
+“"Go, stand in the temple and speak to the people all the __ of this __."” (5:20)
+(A) Nations, world
+(B) World, life
+(C) Word, life
 
-11. Why couldn't they that came against Peter and John say anything against the lame man's healing?(4:16)
-(A) Because they were afraid of the people
-(B) Because they have been with Jesus
-(C) Because the miracle done through them was evident to all who dwell in Jerusalem, and they could not deny it.
+17. Who called the council and the elders (senate) of the children of Israel to have the Apostles brought from prison to them? (5:21)
+(A) The high priest
+(B) The high priest and those with him
+(C) Captain of the prison guards
+
+18. When the High Priest, the captain of the temple and the chief Priests heard that the Apostle's were no longer in prison-where were they told that the
+Apostle's were?
+(A) In the Upper Room
+(B) Temple
+(C) Cave
+
+19. “And daily in the __, and in every __, they did not cease teaching and preaching Jesus as the Christ.” (5:42)
+(A) Lord, house
+(B) Temple, house
+(C) Lord, temple
+
+20. How did the Apostles depart from the presence of the council? (5:41)
+(A) They departed with anger
+(B) The Spirit of the Lord took them by cloud
+(C) They departed from the presence of the council, rejoicing that they were counted worthy to suffer shame for His name.”
   
-12. What did the one's who were against Peter and John commanded them not to do?(4:17)
-(A) They commanded them not to heal with the name again
-(B) They commanded them not to have fellowship with the believers again
-(C) They commanded them not to speak at all nor teach in the name of Jesus.
+21. Gamaliel talked about two separate men who had drawn people away with their false doctrine which nothing came of it: What were these two men's names? (5:36-37)
+(A) Thadeus, Judas of Galilee
+(B) Theudas, Judas of Galilee
+(C) Thadeus, Judas son of Alpheus
 
-13. For we cannot but speak the things which we have ___ 
-and ___. (4:20)
-(A) Seen, heard
-(B) Touch, felt
-(C) seen, believed
+22. What was Gamaliel's profession? (5:34)
+(A) A judge
+(B) A priest
+(C) A teacher of the Law
 
-14. The lame man who was healed was about how old?
-(A) Over forty years
-(B) Forty years old
-(C) Thirty years old
+23. What sect was Gamaliel? (5:34)
+(A) A Pharisee
+(B) A Sadducee
+(C) An Essene
 
-15. Why did the nations __, And the people ___vain things?(4:25)
-(A) Rage, plot
-(B) Plan, plot
-(C) Plot, plan
+24. When the Apostles were brought from the temple by the captain and officers, why did they bring the Apostles without violence?(5:26)
+(A) They wanted to seek peace
+(B) They feared the people.
+(C) The Apostles were honourable before their eyes
 
-16. What servant did Peter and John say said that scripture? (4:25)
-(A) Moses
-(B) Isaiah
-(C) David
+25. What is the first word of this chapter?(5:1)
+(A) But
+(B) Now
+(C) And
 
-17. What two people did Peter and John say was against the Holy Servant Jesus whom The Lord has anointed?(4:27)
-(A) Herod and Pontius Pilate
-(B) Ceasar and Pontius Pilate
-(C) Herod and Ceasar
+26. How much money did Ananias lay at the feet of the Apostles?(5:2)
+(A) A certain part of their money
+(B) Few amount of money
+(C) A small part of their money
 
-18. What two groups of people did Peter and John say was against thy Holy Servant Jesus whom The Lord has anointed? (4:27)
-(A) The Pharisees and Sadducees
-(B) The gentiles and the people of Israel
-(C) The gentiles and Pharisees
+27. Ananias' greatest sin was that made him to die was...? (5:3)
+(A) He Lying to the Holy Spirit
+(B) Not laying all the sold land money at feet of the Apostle
+(C) Selling his land
 
-19. What did Peter and John ask God to stretch forth and heal with?(4:30)
-(A) His Hand
-(B) His Rod
-(C) His Finger
+28. Tell me whether you sold the land for so much?" (5:8). Who asked this question?
+(A) Apostle Peter
+(B) Apostle John
+(C) High Priest Annas
 
-20. How did Peter & John speak the word of God?(4:31)
-(A) With eloquence
-(B) With boldness
-(C) With other tongues
+29. To whom was this question asked? (5:8)
+(A) Ananias
+(B) The wife of Ananias
+(C) Barnabas
 
-21. “Now the multitude of those who believed were of one heart and one soul; neither did anyone say that any of the things he possessed was his own, but they had all things in ____.”(4:32)
-(A) Abundance
-(B) Common
-(C) They needed
-
-22. The multitude also with great power, gave the Apostles great witness of the resurrection of the Lord Jesus Christ and what was upon them all?(4:33)
-(A) Boldness
-(B) Great grace
-(C) Mercy
-
-23. Joses was surnamed what by the Apostles?(4:36)
-(A) Barnabas
-(B) Barabbas
-(C) Barsabbas
-
-24. How was his surname interpreted?(4:36)
-(A) King amongst Princes
-(B) Prince of encouragement
-(C) Son of encouragement
-
-25. What was Joses? (nationality)(4:36)
-(A) The country of Greece
-(B) The country of Cyprus
-(C) None of the above
-
-26. Joses was a ____(4:36)
-(A) Levite
-(B) Benjamite
-(C) Gadite
-
-27. What did Joses lay at the Apostle's feet? (4:37)
-(A) Food and clothing
-(B) His sins
-(C) Money he got from selling his land
-
-28. This is not in any name in Heaven or on Earth, except Jesus. (4:12)
-(A) Hope
-(B) Tribulation
-(C) Salvation
-
-29. The following happened when the disciples prayed?(4:31)
-(A) They were filled with the Holy Spirit
-(B) The place they assembled was shaken
-(C) Both of the options above
-
-30. How many verses are in this chapter?
-(A) 32
-(B) 35
-(C) 37
-
-31. What are the last two words of this chapter?
-(A) Were sold
-(B) Apostles' feet
+30. Tell me whether You sold the land for so much?"
+What was the reply?(5:8)
+(A) Yes, for so much
+(B) Yes, We kept none for ourselves
 (C) None of the options above
+
+31. Why did the captain and the officers fear the people?(5:26)
+(A) They feared the people will stone them
+(B) They feared for the people were many
+(C) They feared the people for their courage and boldness
+
+32. For what did the apostles rejoice? (5:42)
+(A) They were counted worthy to suffer shame for the name
+(B) The many that were added to the church
+(C) That their names were written in the Lamb’s book of life
+
+33. What were the apostles accused of filling Jerusalem with? (5:28)
+(A) New converts
+(B) Their doctrine
+(C) Miracles, signs and wonders
+
+34. By what unusual means did people try to obtain healing? (5:15)
+(A) Having Peter’s shadow passing over them
+(C) Having handerchiefs brought to them from the apostles
+(C) Touching the hem of the apostles’ garments
+
+35. When the officers came to prison the apostles were __(5:22)
+(A) Worshiping God
+(B) Gone
+(C) Teaching and preaching to the people
+
+36. Peter and the other apostles answered the council saying___(5:29)
+(A) We ought to obey God rather than man
+(B) An angel delivered us from prison
+(C) Please do not put us back in prison
+
+37. In what type prison were the apostles jailed?(5:18)
+(A) Maximum security
+(B) Common prison
+(C) Workhouse
+
+38. All the apostles were with one accord in____(5:12)
+(A) Solomon's porch
+(B) Upper Room
+(C) The holy spirit
 
  -->
