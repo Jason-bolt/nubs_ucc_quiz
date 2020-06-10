6ba66d6d07367e2e30f0689600b976a4683ba182
Jason-bolt <58566161+Jason-bolt@users.noreply.github.com>
2020-06-10 10:01:06 +0000

    Add files via upload

--- public/db.php
+++ public/db.php
@@ -1,25 +1,9 @@
 <?php
 
-	// define("DB_HOST", "remotemysql.com");
-	// define("DB_USER", "5LjmMscBgr");
-	// define("DB_PASSWORD", "01lIYfVvyy");
-	// define("DB_NAME", "5LjmMscBgr");
-
-	// $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-	// if (mysqli_connect_error()) {
-	// 	die("Database connection failed "
-	// 		.
-	// 	 mysqli_connect_error()
-	// 	);
-	// }
-
-
-
-	define("DB_HOST", "localhost");
-	define("DB_USER", "root");
-	define("DB_PASSWORD", "");
-	define("DB_NAME", "nubs_ucc_quiz");
+	define("DB_HOST", "remotemysql.com");
+	define("DB_USER", "5LjmMscBgr");
+	define("DB_PASSWORD", "01lIYfVvyy");
+	define("DB_NAME", "5LjmMscBgr");
 
 	$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
@@ -29,5 +13,21 @@
 		 mysqli_connect_error()
 		);
 	}
+
+
+
+	// define("DB_HOST", "localhost");
+	// define("DB_USER", "root");
+	// define("DB_PASSWORD", "");
+	// define("DB_NAME", "nubs_ucc_quiz");
+
+	// $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+	// if (mysqli_connect_error()) {
+	// 	die("Database connection failed "
+	// 		.
+	// 	 mysqli_connect_error()
+	// 	);
+	// }
 
 ?>
