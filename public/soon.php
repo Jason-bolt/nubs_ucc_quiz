9620e57254630bb2773af94331e95e1fe84e486f
Jason-bolt <58566161+Jason-bolt@users.noreply.github.com>
2020-07-06 20:53:34 +0000

    Add files via upload

--- public/soon.php
+++ public/soon.php
@@ -24,7 +24,7 @@
 
 <section class="container py-5 text-center">
 	<div class="card py-4">
-		<h5>Quiz on Romans Chapter 6 will start at 10am</h5>
+		<h5>Quiz on Romans Chapter 7 will start at 10am</h5>
 		<!-- <h5>The quiz is temporarily halted and will resume on Friday</h5> -->
 	</div>
 </section>
